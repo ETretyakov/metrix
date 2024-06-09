4dc71a1e7b2952a637e0288c12bdb68d9b964d2a
Евгений Третьяков <estretyakov@sbercloud.ru>
2024-06-09 12:07:59 +0300

    [iter10] fixed iter 5

--- autotest.sh
+++ autotest.sh
@@ -2,16 +2,21 @@ go build -o cmd/server/server cmd/server/*.go
 go build -o cmd/agent/agent cmd/agent/*.go
 
 ./metricstest -test.v -test.run=^TestIteration1$ \
-    -binary-path=cmd/server/server
+              -binary-path=cmd/server/server
 ./metricstest -test.v -test.run=^TestIteration2[AB]$ \
-    -source-path=. \
-    -agent-binary-path=cmd/agent/agent
+              -source-path=. \
+              -agent-binary-path=cmd/agent/agent
 ./metricstest -test.v -test.run=^TestIteration3[AB]*$ \
-            -source-path=. \
-            -agent-binary-path=cmd/agent/agent \
-            -binary-path=cmd/server/server
+              -source-path=. \
+              -agent-binary-path=cmd/agent/agent \
+              -binary-path=cmd/server/server
 ./metricstest -test.v -test.run=^TestIteration4$ \
-            -agent-binary-path=cmd/agent/agent \
-            -binary-path=cmd/server/server \
-            -server-port=8888 \
-            -source-path=.
+              -agent-binary-path=cmd/agent/agent \
+              -binary-path=cmd/server/server \
+              -server-port=8888 \
+              -source-path=.
+./metricstest -test.v -test.run=^TestIteration5$ \
+              -agent-binary-path=cmd/agent/agent \
+              -binary-path=cmd/server/server \
+              -server-port=8888 \
+              -source-path=.
